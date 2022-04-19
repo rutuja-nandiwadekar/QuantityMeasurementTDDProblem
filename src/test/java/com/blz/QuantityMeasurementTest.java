cfe34b0b3ce53e7b0cf5a51bc25cd0b2d755d1dc
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-04-19 18:59:46 +0530

    Added: TC 1.1 to check equality of 2 feet variables

--- src/test/java/com/blz/QuantityMeasurementTest.java
+++ src/test/java/com/blz/QuantityMeasurementTest.java
@@ -1,6 +1,24 @@
 package com.blz;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class QuantityMeasurementTest {
+    /*
+      TC 1.1 to check equality of 2 feet variables
+    */
+    @Test
+    public void given0FeetAnd0Feet_ShouldReturnEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(0.0);
+        Assert.assertEquals(feet1, feet2);
+    }
+
+    @Test
+    public void given0FeetAnd1Feet_ShouldReturnNotEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(1.0);
+        Assert.assertNotEquals(feet1, feet2);
+    }
 
 }
 
