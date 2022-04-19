45ae1a4cfdcf6cdbebdc7edf2b44b513ec4b65a7
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-04-19 20:02:34 +0530

    Added: TC 1.6 to check equality of 2 inch variables

--- src/test/java/com/blz/QuantityMeasurementTest.java
+++ src/test/java/com/blz/QuantityMeasurementTest.java
@@ -3,6 +3,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class QuantityMeasurementTest {
+    //test case for Feet
     /*
       TC 1.1 to check equality of 2 feet variables
     */
@@ -83,5 +84,26 @@ public class QuantityMeasurementTest {
         feet2.setValue(10);
         Assert.assertNotEquals(feet1.getValue(),feet2.getValue(),0.0);
     }
+
+
+
+    //test case for Inch
+    /*
+      TC 1.6 to check equality of 2 inch variables
+    */
+    @Test
+    public void given0InchAnd0Inch_ShouldReturnEqual() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(0.0);
+        Assert.assertEquals(inch1, inch2);
+    }
+
+    @Test
+    public void given0InchAnd1Inch_ShouldReturnNotEqual() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(1.0);
+        Assert.assertNotEquals(inch1, inch2);
+    }
+
 }
 
