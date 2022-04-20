f5185224560775bbd3e08927e70c987cc4aaa6e9
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-04-20 22:01:51 +0530

    Added: TC 1.15 for comparing lengths 5: 36 in = 1 yd

--- src/test/java/com/blz/QuantityMeasurementTestRefactor.java
+++ src/test/java/com/blz/QuantityMeasurementTestRefactor.java
@@ -200,6 +200,16 @@ public class QuantityMeasurementTestRefactor {
       TC 1.14 for comparing lengths 4: 1 yd = 36 in
      */
     @Test
+    public void given1YardAnd36InchWhenCompared_shouldReturnTrue() {
+        double value2 = quantityMeasurement1.unitComparison(Units.YARD, 1.0);
+        double value1 = quantityMeasurement.unitComparison(Units.INCH, 36.0);
+        Assert.assertEquals(value1,value2,0.0);
+    }
+
+    /*
+      TC 1.15 for comparing lengths 5: 36 in = 1 yd
+     */
+    @Test
     public void given36InchAnd1YardWhenCompared_shouldReturnTrue() {
         double value1 = quantityMeasurement.unitComparison(Units.INCH, 36.0);
         double value2 = quantityMeasurement1.unitComparison(Units.YARD, 1.0);
